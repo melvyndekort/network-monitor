c4f5555839591395384ecf10d186d8ae48e70e8e
Melvyn de Kort <m.de.kort@portbase.com>
2026-03-15 17:01:53 +0100

    Add UI deployment workflow with full S3 sync

--- scripts/deploy_ui.sh
+++ scripts/deploy_ui.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Deploy UI to S3
 aws s3 sync ui/ s3://network-monitor-ui-844347863910/ \
-  --content-type "text/html" \
+  --delete \
   --region eu-west-1
 
 echo "UI deployed to http://network-monitor-ui-844347863910.s3-website-eu-west-1.amazonaws.com"
